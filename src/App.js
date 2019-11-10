ff1175ac8565f1a78b1c0984604b2ea73279659b
Lucas <lhamancio@gmail.com>
2019-11-10 19:27:07 -0300

    GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <div className="App">hello word</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
