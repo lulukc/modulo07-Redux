2d7eb7d9121cc2c7a226758f1dcf5a0d2e3df9c5
Lucas <lhamancio@gmail.com>
2019-11-13 21:27:48 -0300

    style cart and api conecton

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito legal</strong>
+              <span>RS 379,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$759,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button"> Finalizar pedido </button>
+        <Total>
+          <span>Total</span>
+          <strong>R$759,90</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format'
 
 import { ProductList } from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/57f27d11b18f4a8d9d9aa7fa00ed0c3d_9366/Tenis_Superstar_Branco_C77124_07_standard.jpg"
-          alt="Tenis"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>RS 379,90</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }))
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
