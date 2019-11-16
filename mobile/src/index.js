f0fc56663a92cade4af2b3888092a167d18880c3
Lucas <lhamancio@gmail.com>
2019-11-16 12:46:01 -0300

    mobile app start

---
+++ mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+    </>
+  );
+}
